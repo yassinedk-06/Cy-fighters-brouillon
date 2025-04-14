24fafa47b5b5b52507628615d4ad917a1a71cd81
charbeleliass <eliascharbel05@hotmail.com>
2025-04-14 13:20:43 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,4 @@
 #include "menu.h"
-#ifndef COULEURS_H
-#define COULEURS_H
-#define ROUGE   "\033[1;31m"
-#define VERT    "\033[1;32m"
-#define JAUNE   "\033[1;33m"
-#define BLEU    "\033[1;36m"
-#define RESET   "\033[0m"
-
-#endif
 
 int main() {
     demarrerJeu();
