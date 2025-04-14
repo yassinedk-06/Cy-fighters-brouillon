5747957af9157cf8d522bcaef7e4542b9971cbc8
charbeleliass <eliascharbel05@hotmail.com>
2025-04-14 12:33:55 +0200

    Create combattant.c

---
+++ combattant.c
@@ -0,0 +1,14 @@
+#include "combattant.h"
+
+Player p1 = {"luffy", 70, PVMAX, 0.3, 7, 2, 0.4, ENNERGY, "Gum Gum no Gum Gum", 1.4, 2, "Gear 5", 1.8, 4};
+Player p2 = {"sanji", 60, PVMAX, 0.4, 8, 3, 0.2, ENNERGY, "diabolo jumbo", 1.2, 2, "bien cuit grill shot", 1.4, 4};
+Player p3 = {"zoro", 80, PVMAX, 0.1, 7, 4, 0.3, ENNERGY, "tiger slash", 1.1, 2, "oni giri", 1.2, 4};
+Player p4 = {"ace", 90, PVMAX, 0.3, 5, 1, 0.5, ENNERGY, "Flame fence", 1.2, 1, "Heat haze", 1.4, 2};
+Player p5 = {"brook", 70, PVMAX, 0.4, 5, 4, 0.3, ENNERGY, "Song slash", 1.2, 3, "blizzard slice", 1.4, 6};
+Player p6 = {"ussop", 80, PVMAX, 0.3, 5, 3, 0.4, ENNERGY, "Skull bomb blast", 1.1, 1, "Platanus Shuriken", 1.2, 2};
+Player p7 = {"do flamingo", 60, PVMAX, 0.4, 5, 5, 0.3, ENNERGY, "Fallbright", 1.2, 2, "String saw", 1.4, 4};
+Player p8 = {"big mom", 80, PVMAX, 0.3, 6, 1, 0.5, ENNERGY, "Master cannon", 1.3, 1, "Mama raid", 1.6, 2};
+Player p9 = {"katakuri", 80, PVMAX, 0.4, 8, 1, 0.2, ENNERGY, "Mochi hadan", 1.3, 3, "Zan Giri Mochi", 1.6, 6};
+Player p10 = {"black beard", 80, PVMAX, 0.1, 7, 4, 0.3, ENNERGY, "Black hole", 1.4, 2, "Black vortex", 1.8, 4};
+Player p11 = {"crocodile", 60, PVMAX, 0.5, 4, 5, 0.3, ENNERGY, "Desert spada", 1.2, 3, "Ground death", 1.4, 6};
+Player p12 = {"Kaido", 90, PVMAX, 0.3, 8, 4, 0.2, ENNERGY, "Thunder", 1.4, 2, "Blast breath", 1.8, 4};
