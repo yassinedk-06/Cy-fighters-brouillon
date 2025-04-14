2827fadf1a5acdba4cf8fa0f3507a4c62b53294b
charbeleliass <eliascharbel05@hotmail.com>
2025-04-14 12:29:02 +0200

    Create armes.c

---
+++ armes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/assets.h"
+
+void atout(Player* p, Assets a) {
+    switch(a) {
+        case Bouclier:
+            p->def += 0.1;
+            p->dodge -= 0.1;
+            break;
+        case Epee:
+            p->att += 0.1;
+            p->def -= 0.1;
+            break;
+        case Mitraillette:
+            p->crit1 += 0.1;
+            p->crit2 += 0.1;
+            p->pv -= 10;
+            break;
+        case Baton:
+            p->dodge += 0.1;
+            p->crit1 -= 0.1;
+            p->crit2 -= 0.1;
+            break;
+        case Medkit:
+            p->pv += 10;
+            p->att -= 0.1;
+            break;
+        case Eclair:
+            p->speed += 1;
+            p->att -= 0.1;
+            break;
+        case None:
+            break;
+        default:
+            printf("error : atout non reconnu \n");
+            exit(1);
+    }
+}
