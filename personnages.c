7fb1c31f332e040dac58e950ef3a63f147f37161
charbeleliass <eliascharbel05@hotmail.com>
2025-04-14 12:32:10 +0200

    Create personnages.c

---
+++ personnages.c
@@ -0,0 +1,16 @@
+#include "../include/personnage_affichage.h"
+
+Personnage persos[12] = {
+    {"(1)  Luffy", 2, 4, 3, 2, 4, 4},
+    {"(2)  Sanji", 1, 5, 4, 3, 2, 2},
+    {"(3)  Zoro", 3, 4, 1, 4, 3, 1},
+    {"(4)  Ace", 4, 2, 3, 1, 5, 2},
+    {"(5)  Brook", 2, 2, 4, 4, 3, 2},
+    {"(6)  Ussop", 3, 2, 3, 3, 4, 1},
+    {"(7)  Do Flamingo", 1, 2, 4, 5, 3, 2},
+    {"(8)  Big Mom", 3, 3, 3, 1, 5, 3},
+    {"(9)  Katakuri", 3, 5, 4, 1, 2, 3},
+    {"(10) Black Beard", 3, 4, 1, 4, 3, 4},
+    {"(11) Crocodile", 1, 1, 5, 5, 3, 2},
+    {"(12) Kaido", 4, 5, 3, 4, 2, 4}
+};
